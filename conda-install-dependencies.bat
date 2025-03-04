ce28ee550b0f49cb3683117eb6d3dc7056732bd3
svetad <sv.dmytr@gmail.com>
2025-03-04 12:30:11 +0200

    Added STATIC build

--- conda-install-dependencies.bat
+++ conda-install-dependencies.bat
@@ -13,7 +13,7 @@ git clone  --recurse-submodules https://github.com/sdmytrievs/arango-cpp.git
 cd arango-cpp
 
 echo "Configuring..."
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -DBUILD_EXAMPLES=OFF  -DBULID_LOCAL_TESTS=OFF -DBULID_REMOTE_TESTS=OFF  .. -A x64 -S . -B build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -DBUILD_EXAMPLES=OFF  -DBULID_LOCAL_TESTS=OFF -DBULID_REMOTE_TESTS=OFF  -A x64 -S . -B build
 echo "Building..."
 cmake --build build --target install
 
@@ -30,7 +30,7 @@ git clone https://github.com/sdmytrievs/jsonio.git
 cd jsonio
 
 echo "Configuring..."
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -DBuildExamples=OFF  -DBuildTests=OFF .. -A x64 -S . -B build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -DBuildExamples=OFF  -DBuildTests=OFF -A x64 -S . -B build
 echo "Building..."
 cmake --build build --target install
 
